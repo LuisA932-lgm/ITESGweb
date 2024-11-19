a6585a4908b1e63f168166bee00951c79da177cd
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-19 15:05:10 -0600

    I1

--- sistemas/Informacion_Financiera/2023.php
+++ sistemas/Informacion_Financiera/2023.php
@@ -1,1 +1,20 @@
-HOLA
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Ejercicios Fiscales 2021</title>
+  </head>
+  <body>
+  <style>
+       .Ti {
+           
+            color: rgb(181, 129, 45);
+            font-family: Lato, sans-serif;
+            font-size: 21px;
+            font-weight:bold;
+       }
+       </style>
+    <center class="Ti">Información Financiera - Armonización Contable LGCG y LDF</center>
+</body>
+</html>
