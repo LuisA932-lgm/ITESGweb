7757c98b40e7648fd904133cd5d537587fb79445
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-19 13:30:37 -0600

    D1

--- sistemas/views/hola.php
+++ sistemas/views/hola.php
@@ -224,12 +224,12 @@ fieldset[disabled] .btn-primary:hover {
           <div class="btn-group btn-group-lg" role="group">
    
           <a href="http://localhost/ITESGWEB/sistemas/views/hola.php"><button type="button" style="color: #2EC3F8; font-size: 22px " class="  btn btn-default">2018</button>
-          </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php?option=com_content&amp;view=article&amp;id=75&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class=" btn btn-default">2019</button>
-          </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php?option=com_content&amp;view=article&amp;id=79&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2020</button>
-            </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php?option=com_content&amp;view=article&amp;id=87&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2021</button></a>
-          <a href="http://localhost/ITESGWEB/sistemas/views/hola.php?option=com_content&amp;view=article&amp;id=92&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2022</button></a>
+          </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class=" btn btn-default">2019</button>
+          </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2020</button>
+            </a><a href="http://localhost/ITESGWEB/sistemas/views/hola.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2021</button></a>
+          <a href="http://localhost/ITESGWEB/sistemas/views/hola.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2022</button></a>
            <a href="http://localhost/ITESGWEB/sistemas/Informacion_Financiera/2023.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2023</button></a>
-          <a href="http://localhost/ITESGWEB/sistemas/views/hola.php?option=com_content&view=article&id=104&Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2024</button></a>
+          <a href="http://localhost/ITESGWEB/sistemas/Informacion_Financiera/2024.php"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2024</button></a>
           </div>
   </center>
   <div>
