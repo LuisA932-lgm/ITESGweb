eb5c7a2a566c821eca0b571ef108ca500c1161f0
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-19 13:08:31 -0600

    D1

--- sistemas/views/hola.php
+++ sistemas/views/hola.php
@@ -7,7 +7,8 @@ include ("header.php");
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+      <title>Ejercicios Fiscales 2021</title>
+
   </head>
   <body>
     <!-- <style>
