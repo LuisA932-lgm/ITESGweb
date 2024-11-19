89d883e1e53123b5087f6a450798b567f7d4007f
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-19 15:20:23 -0600

    d1

--- sistemas/Informacion_Financiera/2023.php
+++ sistemas/Informacion_Financiera/2023.php
@@ -16,6 +16,6 @@
 </style>
 <body>
     <h2 class="sub">Información Anual 2023</h2>
-    <h3><a>Liga de Cuenta Pública 2023 (SFIyA)</a></h3>
+    <a href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank"><h3 style="color:  #095ba3 ">Liga de Cuenta Pública 2023 (SFIyA)</h3></a>
 </body>
 </html>
