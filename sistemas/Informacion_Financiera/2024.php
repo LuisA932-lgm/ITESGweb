3713101cf2b67704528d1c3d9884d818dc3473ff
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-19 15:28:48 -0600

    f1

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -19,12 +19,18 @@
         margin-top: -25px;
         margin-left: 10px;
     }
+    .Trans{
+        color:black;
+        font-size: 15px;
+        margin-left: 10px;
+        margin-top: -20px;
+    }
 </style>
 <body>
     <h2 class="sub">Información Anual</h2>
         <a  href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank">
     <h3 class="lin" style="color:  #095ba3 ">Liga de Cuenta Pública 2024 (SFIyA)</h3></a>
-    <h5>Transparencia y Difusión de la Información Financiera LGCG</h5>
+    <h5 class="Trans">Transparencia y Difusión de la Información Financiera LGCG</h5>
     
 </body>
 </html>
