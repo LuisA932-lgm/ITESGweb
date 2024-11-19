89337e4f2446f5dc72707e2dd98fc14bfc7352ce
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-19 12:09:32 -0600

    c3

--- sistemas/views/hola.php
+++ sistemas/views/hola.php
@@ -137,7 +137,7 @@ fieldset[disabled] .btn-default:hover {
   <center>   
           <div class="btn-group btn-group-lg" role="group">
    
-          <a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=68&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px " class="  btn btn-default">2018</button>
+          <a href="http://www.itesg.edu.mx/hola.php?option=com_content&amp;view=article&amp;id=68&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px " class="  btn btn-default">2018</button>
           </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=75&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class=" btn btn-default">2019</button>
           </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=79&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2020</button>
            </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=87&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2021</button></a>
