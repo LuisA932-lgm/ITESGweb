358f05da783960929a5f07f729c81dfd3d8a0247
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-19 15:05:09 -0600

    d3

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -1,1 +1,20 @@
-ITESG
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informacion Financiera</title>
+</head>
+<style>
+    .sub{
+        font-size: 24px;
+        text-align: center;
+        font-weight: bold;
+        margin-bottom: 20px;
+    }
+</style>
+<body>
+    <h2 class="sub">Información Anual</h2>
+</body>
+</html>
+
