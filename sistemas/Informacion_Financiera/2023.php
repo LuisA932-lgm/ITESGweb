96639799a04b8833f828f679a1d7d29de201a713
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-19 15:16:09 -0600

    D1

--- sistemas/Informacion_Financiera/2023.php
+++ sistemas/Informacion_Financiera/2023.php
@@ -3,19 +3,18 @@
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ejercicios Fiscales 2021</title>
+      <title>Ejercicios Fiscales 2023</title>
   </head>
   <body>
   <style>
     .sub{
         color: rgb(181, 129, 45);
         font-size: 24px;
-        text-align: center;
         font-weight: bold;
         margin-bottom: 20px;
     }
 </style>
 <body>
-    <h2 class="sub">Información Anual</h2>
+    <h2 class="sub">Información Anual 2023</h2>
 </body>
 </html>
