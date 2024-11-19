6b8a67c494007598697a49618462bef1ea1ae6f6
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-19 12:05:51 -0600

    c3

--- sistemas/views/hola.php
+++ sistemas/views/hola.php
@@ -18,16 +18,7 @@ include ("header.php");
             font-size: 21px;
             font-weight:bold;
        }
-       .uno:hover{
-        background-color: transparent;
-        color: #12C;
-          }
-          .dos:hover{
-        background-color: transparent;
-        color: #12C;
-        border-color: #fff;
-        
-          }
+     
           .btn {
     display: inline-block;
     margin-bottom: 0;
@@ -144,8 +135,8 @@ fieldset[disabled] .btn-default:hover {
   <center>   
           <div class="btn-group btn-group-lg" role="group">
    
-          <a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=68&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="uno  btn btn-default">2018</button>
-          </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=75&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="dos btn btn-default">2019</button>
+          <a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=68&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px " class="  btn btn-default">2018</button>
+          </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=75&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class=" btn btn-default">2019</button>
           </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=79&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2020</button>
            </a><a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=87&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2021</button></a>
           <a href="http://www.itesg.edu.mx/index.php?option=com_content&amp;view=article&amp;id=92&amp;Itemid=572"><button type="button" style="color: #2EC3F8; font-size: 22px" class="btn btn-default">2022</button></a>
