f47b31b067b929565519769584d42fc7d46c9979
Shiza <cesarajc2002@gmail.com>
2024-11-20 11:13:32 -0600

    Update administrativo.php

--- sistemas/views/institucion/directorio/administrativo.php
+++ sistemas/views/institucion/directorio/administrativo.php
@@ -1,5 +1,5 @@
-<php?
-  include '/sistemas/views/header.php';
+  <?php
+    include '/sistemas/views/header.php';
   ?>
 <body>
     <h1 align="center">Directorio</h1>
@@ -269,6 +269,6 @@
     </table>';
     ?>
 </body>
-  <php?
+  <?php
     include '/sistemas/views/footer.php';
   ?>
