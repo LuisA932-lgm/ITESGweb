f7024b905dc13fd36a3c457ed0ae1c67bb24f627
Shiza <cesarajc2002@gmail.com>
2024-11-20 11:12:45 -0600

    Update 2024-2025.php

--- sistemas/views/institucion/calendario/2024-2025.php
+++ sistemas/views/institucion/calendario/2024-2025.php
@@ -1,6 +1,6 @@
-<?php
-include("header.php");
-?>
+  <?php
+    include '/sistemas/views/header.php';
+  ?>
 
 <body>
     <div class="text-center">
@@ -14,6 +14,6 @@ include("header.php");
         </a>
     </div>
 </body>
-<?php
-include("footer.php");
-?>
+  <?php
+    include '/sistemas/views/footer.php';
+  ?>
