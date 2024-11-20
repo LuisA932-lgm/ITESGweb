fd6fb4571ecdc912b5212f34a0b663ea1e0bd3bd
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-20 10:23:59 -0600

    c11

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +13,7 @@
 
 </head>
 <style>
-    .sub{
+    .sub {
         color: rgb(181, 129, 45);
         font-size: 24px;
         text-align: left;
@@ -20,89 +21,150 @@
         margin-bottom: 20px;
         margin-left: 15px;
     }
-    .lin{
+
+    .lin {
         font-size: 20px;
         margin-top: -25px;
         margin-left: 15px;
         font-weight: bold;
     }
-    .Trans{
-        color:black;
+
+    .Trans {
+        color: black;
         font-size: 15px;
         margin-left: 15px;
         margin-top: -20px;
         font-weight: bold;
     }
-    .ing{
+
+    .ing {
         font-size: 24px;
         margin-left: 15px;
         margin-top: -20px;
         font-weight: bold;
     }
-    .Egre{
+
+    .Egre {
         font-size: 24px;
         margin-left: 15px;
         margin-top: -20px;
         font-weight: bold;
     }
-    .table{
+
+    .table1, table2 {
         width: 800px;
         border-collapse: collapse;
         border-radius: 5px;
         margin-top: 20px;
         margin-left: 300px;
     }
-
+   
 </style>
+
 <body>
     <h2 class="sub">Información Anual</h2>
-        <a  href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank">
-    
-        <h3 class="lin" style="color:  #095ba3 ">Liga de Cuenta Pública 2024 (SFIyA)</h3></a>
-   
-        <h5 class="Trans">Transparencia y Difusión de la Información Financiera LGCG</h5>
-        
-        <h3 class="ing" style="color: #FB008A">Ingreso</h3>
+    <a href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank">
 
-        <table width="70%" align="center" class="table ">
-                <thead style="background-color: #0073D1; color: #FFFFFF"><tr><th>Documento</th><th>Descarga</th></tr></thead>
-            <tbody>
-                    <tr style="background-color: #f8f8f9;">
-                        <td style="width: 80%;">Iniciativa de Ingresos</td>
-                        <td style="width: 20%;">
-                               <a href="/LGCGyLDF/EJ2024/Iniciativa_de_Ingresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="width: 80%;">Información Adicional a la Iniciativa de Ingresos</td>
-                       <td style="width: 20%;">
-                             <a href="/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
-                            <a href="/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
-                        </td>
-                    </tr>
-                    <tr style="background-color: #f8f8f9;">
-                        <td style="width: 80%;">Dictámenes Acuerdo y Actas </td>
-                        <td style="width: 20%;">
-                         <a href="/LGCGyLDF/EJ2024/Dictamenes_Acuerdo_y_Actas_ingreso_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="width: 80%;">Ley de Ingresos </td>
-                       <td style="width: 20%;">
-                           <a href="/LGCGyLDF/EJ2024/Ley_de_Ingresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
-                        </td>
-                    </tr>
-                    <tr style="background-color: #f8f8f9;">
-                        <td style="width: 80%;">Calendario de Ingresos</td>
-                        <td style="width: 20%;">
-                            <a href="/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
-                            <a href="/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
-                        </td>
-                    </tr>
-            </tbody>
-        </table>
+        <h3 class="lin" style="color:  #095ba3 ">Liga de Cuenta Pública 2024 (SFIyA)</h3>
+    </a>
+
+    <h5 class="Trans">Transparencia y Difusión de la Información Financiera LGCG</h5>
+
+    <h3 class="ing" style="color: #FB008A">Ingreso</h3>
+
+    <table width="70%" align="center" class="table1 ">
+        <thead style="background-color: #0073D1; color: #FFFFFF">
+            <tr>
+                <th>Documento</th>
+                <th>Descarga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Iniciativa de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Iniciativa_de_Ingresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%;">Información Adicional a la Iniciativa de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Dictámenes Acuerdo y Actas </td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Dictamenes_Acuerdo_y_Actas_ingreso_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%;">Ley de Ingresos </td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Ley_de_Ingresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Calendario de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
     <h3 class="Egre" style="color: #FB008A">Egreso</h3>
-</body>
-</html>
 
+    <table align="center" width="70%" class="table2">
+        <thead style="background-color: #0073D1; color: #FFFFFF">
+            <tr>
+                <th>Documento</th>
+                <th>Descarga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Proyecto de presupuesto de egresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Proyecto_de_presupuesto_de_egresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%;">Información Adicional al proyecto de presupuesto de egresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/3.INF.%20ADIC.%20PROYECTO%20PPTO.%20EGRESOS%202024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="/LGCGyLDF/EJ2024/3.INF.%20ADIC.%20PROYECTO%20PPTO.%20EGRESOS%202024.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Dictámenes Acuerdo y Actas </td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Dictamenes_Acuerdo_y_Actas_egreso_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%;">Presupuesto de Egresos</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/Presupuesto_de_Egresos_2024.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%;">Calendario de Egreso</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/4.CALENDARIO%20DE%20EGRESOS%202023.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="/LGCGyLDF/EJ2024/4.CALENDARIO%20DE%20EGRESOS%202023.xlsx" target="_blank"> <img src="images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%;">Presupuesto Ciudadano</td>
+                <td style="width: 20%;">
+                    <a href="/LGCGyLDF/EJ2024/PresupuestoCiudadano-2.pdf" target="_blank"> <img src="images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
