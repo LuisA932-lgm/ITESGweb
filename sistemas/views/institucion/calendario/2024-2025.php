b4a8838ca313c96f041eab4449402bc24c94aefd
Shiza <cesarajc2002@gmail.com>
2024-11-20 08:34:49 -0600

    Create 2024-2025.php

---
+++ sistemas/views/institucion/calendario/2024-2025.php
@@ -0,0 +1,19 @@
+<?php
+include("header.php");
+?>
+
+<body>
+    <div class="text-center">
+        <img src='../images/recursos/cal_esc_itsg24_25.jpg' class="w-75  mx-auto">
+    </div>
+    <h1 style="font-size:200%;">CALENDARIO 2024-2025 ITESG</h1>
+    <div class="alert alert-success mx-4">
+        <a href="../images/documentos/cal_esc_itsg24_25.pdf" download="calendario 2024-2025 ITESG.pdf" target="_blank">
+            <img src="/ITESG/images/desc_pdf.png" width="44" height="50"> 
+            <p> Descarga el calendario</p>
+        </a>
+    </div>
+</body>
+<?php
+include("footer.php");
+?>
