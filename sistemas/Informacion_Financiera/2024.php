132357ccdf8a3b30511266b5909f4755b9192a98
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-20 09:59:49 -0600

    c5

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -40,12 +40,15 @@
         font-weight: bold;
     }
     .table1{
-        width: 1000px;
+        width: 900px;
         border-collapse: collapse;
         border-radius: 5px;
         margin-top: 20px;
         margin-left: 50px;
        
+    }
+    .Doc1{
+        margin-left: 3px;
     }
 
 </style>
@@ -59,8 +62,8 @@
         
         <h3 class="ing" style="color: #FB008A">Ingreso</h3>
 
-        <table width="70%" align="center" class="table1 ">
-                <thead style="background-color: #0073D1; color: #FFFFFF"><tr><th>Documento</th><th>Descarga</th></tr></thead>
+        <table width="70%" align="center" class="table1 table-condensed table-striped">
+                <thead class="Doc1" style="background-color: #0073D1; color: #FFFFFF"><tr><th>Documento</th><th>Descarga</th></tr></thead>
             <tbody>
                     <tr>
                         <td style="width: 80%;">Iniciativa de Ingresos</td>
