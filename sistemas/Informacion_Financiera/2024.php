1a60781136240afcdfc454649d3a0e55f4eb16de
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-20 09:43:23 -0600

    c5

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informacion Financiera</title>
     <link rel="stylesheet" href="../sistema/css/gobmx.css">
+    <link rel="stylesheet" href="../sistema/css/style.css">
+
 </head>
 <style>
     .sub{
