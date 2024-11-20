c915d70b174c1180a0e4fb5cd85b2a6308c99ab0
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-11-20 09:49:46 -0600

    c5

--- sistemas/Informacion_Financiera/2024.php
+++ sistemas/Informacion_Financiera/2024.php
@@ -40,7 +40,7 @@
         font-weight: bold;
     }
     .table{
-        width: 60px;
+        width: 150px;
         border-collapse: collapse;
         border-radius: 5px;
         margin-top: 20px;
