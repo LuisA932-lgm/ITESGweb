96f3e1bfcab4b0214aa9a3036c094830d81b0e51
Shiza <cesarajc2002@gmail.com>
2024-11-21 10:02:27 -0600

    Add files via upload

---
+++ sistemas/views/comite_interno.php
@@ -0,0 +1,258 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/header.php';
+?>
+
+<div class="block">
+
+    <header class="page-header">
+        <h1 class="page-title">
+            Artículos </h1>
+    </header>
+    <article class="article fulltext ">
+        <section class="article-body">
+            <p align="center"><img src="/sistemas/images/comite/titulo_ciilynd.png" alt="Titulo_ILyND"></p><br>
+
+            <left>
+                <h2>Introducción.</h2>
+            </left><br>
+            <div align="justify">
+                <font face="Arial,Verdana">
+                    <p>En <strong>México</strong>, todas las personas son iguales y gozan de los derechos y libertades
+                        establecidos en la Constitución; además, los tratados internacionales en la materia les
+                        garantizan, sin distinción alguna, el goce de sus derechos civiles, políticos, económicos,
+                        sociales, culturales y ambientales. En este sentido, eliminar la discriminación es una condición
+                        indispensable para hacer de México una sociedad de derechos.<br><br>
+
+                        En congruencia con lo anterior, en abril del año 2018, el <strong>Instituto Tecnológico Superior
+                            de Guanajuato</strong> sometió las condiciones de trabajo que en él prevalecen, a una
+                        evaluación bajo los parámetros establecidos en la norma <strong>NMX-R-025-SCFI-2015</strong> en
+                        Igualdad Laboral y No Discriminación. Lo anterior, dio como resultado la <strong>obtención de la
+                            certificación</strong> en dicha norma.
+                    </p>
+                </font>
+            </div>
+            <font face="Arial,Verdana"><br>
+
+                <p align="center"><img src="/sistemas/images/comite/logo_norma_ilynd.jpg" alt="NM_ILyND" width="28%"
+                        height="28%"></p><br>
+
+            </font>
+            <div align="justify">
+                <font face="Arial,Verdana">En este contexto, La información que encontrará en el presente apartado
+                    muestra los elementos normativos y actividades que el Instituto, bajo la coordinación del Comité,
+                    tiene y realiza para favorecer el desarrollo integral de las y los trabajadores..</font>
+            </div><br>
+
+            <left>
+                <h2>Política y Código.</h2>
+            </left><br>
+            <div align="justify">
+                <font face="Arial,Verdana">
+                    <p>Algunos de los elementos considerados esenciales (requisitos críticos) por la norma, son el
+                        contar con una <u>política de igualdad laboral y no discriminación</u> en el centro de trabajo,
+                        y la existencia y aplicación de un <u>código de ética.</u></p>
+                </font>
+            </div>
+            <font face="Arial,Verdana"><br><br>
+
+                <center>
+                    <h3>Video de política de igualdad.</h3>
+                </center><br>
+                <div style="text-align:center;">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qJmQR0Ql_to"
+                        title="YouTube video player" frameborder="10"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen=""></iframe>
+                </div>
+                <table>
+
+                </table>
+
+                <br><br><br><br>
+
+                <table style="margin: 0 auto;">
+                    <tbody>
+                        <tr>
+                            <td><a href="/sistemas/images/comite/1.1_Politica_iIgualdad_y_No_Discriminacion.pdf" target="_blank">
+                                    <img alt="Política de Igualdad laboral y no discriminación"
+                                        src="/sistemas/images/comite/1_politica.png" width="72" height="98" border="hidden"
+                                        style="hidden">
+                                </a><strong>Política de Igualdad laboral y no discriminación</strong></td>
+
+                            <td><a href="/sistemas/images/comite/2.1_Codigo_de_Etica.pdf"
+                                    target="_blank">
+                                    <img alt="Código de ética" src="/sistemas/images/comite/2_Codigo.png" width="72" height="98"
+                                        border="5" style="hidden">
+                                </a><strong>Código de ética</strong></td>
+                        </tr>
+
+                        <tr>
+                            <td><a href="/sistemas/images/comite/1.2_Valor_igualdad.jpg" target="_blank">
+                                    <img alt="valor_igualdad" src="/sistemas/images/comite/1.2a1.4_Prevenir.png" width="72"
+                                        height="98" border="5" style="hidden">
+                                </a><strong>Valor de la Igualdad</strong></td>
+
+                            <td><a href="/sistemas/images/comite/1.3_Practicas_de_discriminacion.jpg" target="_blank">
+                                    <img alt="Practicas_discrimicacion" src="/sistemas/images/comite/1.2a1.4_Prevenir.png"
+                                        width="72" height="98" border="5" style="hidden">
+                                </a><strong>Practicas de Discriminación</strong></td>
+                        </tr>
+
+                        <tr>
+                            <td><a href="/sistemas/images/comite/1.4_discriminacion_trabajo.jpg" target="_blank">
+                                    <img alt="Discriminiacion_Trabajo" src="/sistemas/images/comite/1.2a1.4_Prevenir.png"
+                                        width="72" height="98" border="5" style="hidden">
+                                </a><strong>Discriminación en el Trabajo</strong></td>
+
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <p> <br><br></p>
+
+                <left>
+                    <h2>Comité Interno de Igualdad Laboral y No Discriminación (CIILyND).</h2>
+                </left><br>
+                <div align="justify">
+                    <font face="Arial,Verdana">
+                        <p>La implementación y vigilancia de la política en el centro de trabajo está a cargo del Comité
+                            Interno de Igualdad Laboral y No Discriminación, por ello este también se considera esencial
+                            (requisito crítico) para la norma.<br><br>
+
+                            El CIILyND se encuentra conformado de la siguiente manera:
+                        </p>
+                    </font>
+                </div>
+                <font face="Arial,Verdana"><br>
+
+                    <div style="text-align:center;">
+                        <img src="/sistemas/images/comite/3_comite_ilynd.jpg"
+                            alt="Cómite de Igualdad Laboral y No Discriminación">
+                    </div>
+                    <table>
+
+                    </table>
+
+                    <p> <br><br><br></p>
+
+                    <table style="margin: 0 auto;">
+                        <tbody>
+                            <tr>
+                                <td><a href="/sistemas/images/comite/3.2_Lineamientos_del_Comite.pdf" target="_blank">
+                                        <img alt="Lineamientos" src="/sistemas/images/comite/3.1_Lineamientos.png" width="72"
+                                            height="98" border="5" style="hidden">
+                                    </a><strong>Lineamientos del comité</strong></td>
+
+                                <td><a href="/sistemas/images/comite/3.4_como_funciona.jpg" target="_blank">
+                                        <img alt="Como funciona" src="/sistemas/images/comite/3.3_como_funciona.png" width="72"
+                                            height="98" border="5" style="hidden">
+                                    </a><strong>Cómo funciona</strong></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <p> <br><br><br></p>
+
+                    <left>
+                        <h2>Clima laboral.</h2>
+                    </left><br>
+                    <div align="justify">
+                        <font face="Arial,Verdana">
+                            <p>El fomentar y dar a conocer la percepción del personal en materia de clima laboral y no
+                                discriminación, es fundamental para el <strong>CIILyND.</strong><br><br>
+
+                                Ello, debido a que esta acción tiene la finalidad de unir esfuerzos en la implementación
+                                de medidas de prevención y mejora continúa; para favorecer el ejercicio igualitario de
+                                libertades, derechos y oportunidades de las personas trabajadoras de nuestra
+                                Institución.
+                            </p>
+                        </font>
+                    </div>
+                    <font face="Arial,Verdana"><br>
+
+
+                        <p> <br><br><br></p>
+
+                        <left>
+                            <h2>Acciones dentro del ITESG.</h2>
+                        </left><br>
+
+                        <table style="margin: 0 auto;">
+                            <tbody>
+                                <tr>
+                                    <td><a href="/sistemas/images/comite/Capacitacion.pdf" target="_blank">
+                                            <img alt="Capacitación" src="/sistemas/images/comite/5_Capacitacion.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Capacitación</strong></td>
+
+                                    <td><a href="/sistemas/images/comite/6.1_decalogo.pdf" target="_blank">
+                                            <img alt="Lenguaje" src="/sistemas/images/comite/6_Lenguaje.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Lenguaje</strong></td>
+                                </tr>
+
+                                <tr>
+                                    <td><a href="/sistemas/images/comite/acciones.pdf" target="_blank">
+                                            <img alt="valor_igualdad" src="/sistemas/images/comite/7_acciones.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Acciones</strong></td>
+
+                                    <td><a href="/sistemas/images/comite/7.3_ Reglamento_lactario.pdf" target="_blank">
+                                            <img alt="Lactario" src="/sistemas/images/comite/7.2_Reg_lactario.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Reglamento Lactario</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+
+                        <p> <br><br><br></p>
+
+                        <left>
+                            <h2>Atención de quejas.</h2>
+                        </left><br>
+
+                        <p align="center"><img src="/sistemas/images/comite/ombusperson_ciilynd.jpg" alt="Ombudsperson"
+                                width="80%" height="60%"></p><br><br>
+
+                        <table style="margin: 0 auto;">
+                            <tbody>
+                                <tr>
+                                    <td><a href="/sistemas/images/comite/X_X_X.pdf" target="_blank">
+                                            <img alt="Prevenir" src="/sistemas/images/comite/6_PREVENIR.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Prevenir</strong></td>
+
+                                    <td><a href="/sistemas/images/comite/6.1.2_protocolo_gto.pdf" target="_blank">
+                                            <img alt="Protocolo" src="/sistemas/images/comite/6.1_protocolo.png" width="72"
+                                                height="98" border="5" style="hidden">
+                                        </a><strong>Protocolo Guanajuato</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </font>
+                </font>
+            </font>
+        </section>
+        <font face="Arial,Verdana">
+            <font face="Arial,Verdana">
+                <font face="Arial,Verdana">
+
+
+
+                </font>
+            </font>
+        </font>
+    </article>
+    <font face="Arial,Verdana">
+        <font face="Arial,Verdana">
+            <font face="Arial,Verdana">
+
+            </font>
+        </font>
+    </font>
+</div>
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/footer.php';
+?>
