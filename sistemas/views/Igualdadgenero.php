70e00e49f6e99922c665df26b8909469474f6a22
DanPonce001 <152035986+DanPonce001@users.noreply.github.com>
2024-11-21 08:45:16 -0600

    Add files via upload

---
+++ sistemas/views/Igualdadgenero.php
@@ -0,0 +1,149 @@
+<?php
+include ("header.php");?>
+<style> 
+      h4 { 
+            color: #b5812d;
+            
+      }
+      h3{
+            color: #039;
+      }
+      .iconos{
+            display: flex; 
+            justify-content: flex-end;
+            list-style: none;
+            padding-top: 30px;
+      }
+
+</style>
+<head> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></head>
+
+<section class="actions" style="padding: 0px;">
+                            <ul class="iconos">
+                                    <li class="print-icon" style="margin-left: 15px;">
+                                        <a href="formulario-imprimir.html" title="Imprimir">
+                                            <i class="fas fa-print" style="color: #24398E;"></i> 
+                                        
+                                    </li>
+                                    <li class="email-icon" style="margin-left: 15px;">
+                                        <a href="formulario-correo.html" title="Enviar correo">
+                                            <i class="fas fa-envelope" style="color: #24398E"></i> 
+                                        </a>
+                                    </li>
+                                </ul>
+</section>
+  
+<div class="text-center"><img src='../images/headers/ID_IGUAL_ DISCRI.png' class="w-65 mx-auto">
+</body>
+
+<td align="center" style="padding-right:30px">
+  <h4 style="text-align: center; font-size: 30px; font-weight: bold; margin-left: 2cm; margin-right: 2cm;">
+Campaña de difusión que se realiza desde el Sistema de Gestión de Igualdad de Género y no Discriminación del TecNM.</h4>
+   
+</td></tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Derechos Humanos</h4><div class="text-center"><img src='../images/display/1SLIDER_Der hum.jpg' class="w-85 mx-auto">
+    <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+<h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Igualdad</h4><<div class=""><img src='../images/display/2SLIDER_Igualdad.jpg' class="w-85 mx-auto">
+      <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+<h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Derechos Sexules y Reproductivos</h4><div class=""><img src='../images/display/3DER_SEX.jpg' class="w-85 mx-auto">
+        <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Nuevas Masculinidades</h4><div class=""><img src='../images/display/4nuevas_masculinidades.jpg' class="w-85 mx-auto">
+        <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Violencia de Género</h4><div class=""><img src='../images/display/5Vio_Gnero.jpg' class="w-85 mx-auto">
+        <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Regla de Integridad de Procesos de Evaluación </h4><div class=""><img src='../images/display/6Regla_de integridad.png' class="w-85 mx-auto">
+        <hr class="dotted"></div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Conflicto de Interés</h4><div class=""><img src='../images/display/7Que_hacer_conflicto.png' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">¿Cuál es la diferencia entre Conflicto de Interés y la actuación bajo Conflicto de Interés?</h4><div class=""><img src='../images/display/8Cual_es_la diferencia.png' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Guía para la identificación y Gestión de Conflicto de Interés</h4><div class=""><img src='../images/display/9Guia_para_la Identificacion.png' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Consentimiento</h4><div class=""><img src='../images/display/11Consentimiento.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Los Derechos Sexuales y Reproductivos</h4><div class=""><img src='../images/display/12DERE_SEX.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Violencia contra Mujeres y Niñas en el ámbito privado</h4><div class=""><img src='../images/display/13 VIOL_muje_nia.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Diversidad Sexual y Derechos Humanos</h4><div class=""><img src='../images/display/14 DIVERSIDAD SEXUAL_DH.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Cartilla Derechos Humanos LGBTTTI </h4><div class=""><img src='../images/display/15Derechos_LGBTTTI.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Cartilla Derechos Trans</h4><div class=""><img src='../images/display/16DERECHOS_TRANS.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Derechos Humanos, Libertad de la Persona</h4><div class=""><img src='../images/display/17DH_LP.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Nuevas Masculinidades</h4><div class=""><img src='../images/display/18Slider_nuevas_masculinidades.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Derecho a la Integridad y Seguridad Personal</h4><div class=""><img src='../images/display/19Integridad_Seguridad.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+<tr><td align="center" style="padding-right:30px">
+  <h4 style="center; font-size: 30px; font-weight: bold; margin-left: 1cm; margin-right: 1cm;">Violencia Emocional</h4><div class=""><img src='../images/display/20violencia_emocional.jpg' class="w-85 mx-auto">
+       <hr class="dotted"> </div></td>
+</tr>
+
+
+</tbody>
+</table>
+<p align="center"><img src="../images/logos/todos.jpg"></p></div>
+</section>
+
+
+<?php
+include ("footer.php");
+?>
