7c2b88710be059a43131378da7510445e7abaa12
Shiza <cesarajc2002@gmail.com>
2024-11-21 12:23:22 -0600

    Update 2024-2025.php

--- sistemas/views/2024-2025.php
+++ sistemas/views/2024-2025.php
@@ -10,9 +10,9 @@ include 'header.php';
             </div>
             <h4 style="font-size:200%;">CALENDARIO 2024-2025 ITESG</h4>
             <div class="alert alert-success mx-4">
-                <img src="/sistemas/images/desc_pdf.png" width="44" height="50">
+                <img src="../images/desc_pdf.png" width="44" height="50">
                 <br>
-                <a href="/sistemas/images/documentos/cal_esc_itsg24_25.pdf" download="calendario 2024-2025 ITESG.pdf"
+                <a href="../images/documentos/cal_esc_itsg24_25.pdf" download="calendario 2024-2025 ITESG.pdf"
                     target="_blank">
                     Descarga el calendario
                 </a>
