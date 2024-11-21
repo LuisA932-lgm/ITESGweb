b38ce00549b5673f1dffc0f681283ce311c0edf6
Shiza <cesarajc2002@gmail.com>
2024-11-21 12:27:35 -0600

    Update comite_etica.php

--- sistemas/views/comite_etica.php
+++ sistemas/views/comite_etica.php
@@ -1,42 +1,42 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/header.php';
+include 'header.php';
 ?>
 
 <body>
     <article class="article fulltext ">
         <section class="article-body">
             <div class="text-center">
-                <img src='/sistemas/images/comite/logo_comite.png' class="mx-auto">
+                <img src='../images/comite/logo_comite.png' class="mx-auto">
             </div>
 
             <div>
-                <img src="/sistemas/images/comite/COM_COD_ETI.jpg">
+                <img src="../images/comite/COM_COD_ETI.jpg">
             </div>
 
             <div class="text-center">
-                <a href="/sistemas/images/comite/cod_etica.pdf" target="_blank">
-                    <img src="/sistemas/images/display/info_ccond.jpg" class="img-full-width">
+                <a href="../images/comite/cod_etica.pdf" target="_blank">
+                    <img src="../images/display/info_ccond.jpg" class="img-full-width">
                 </a>
             </div>
 
             <div>
-                <img src="/sistemas/images/comite/COM_INT_ET.jpg">
+                <img src="../images/comite/COM_INT_ET.jpg">
             </div>
 
             <div class="text-center">
-                <img src="/sistemas/images/comite/BAN_WEB_COM_INT.jpg" class="img-full-width">
+                <img src="../images/comite/BAN_WEB_COM_INT.jpg" class="img-full-width">
             </div>
 
             <div class="d-flex align-items-center my-3">
-                <a href="/sistemas/images/comite/3_4_lineamientos_generales.pdf">
-                    <img src="/sistemas/images/comite/codigo.png" alt="Ícono de Código" class="me-3"
+                <a href="../images/comite/3_4_lineamientos_generales.pdf">
+                    <img src="../images/comite/codigo.png" alt="Ícono de Código" class="me-3"
                         style="width: 70px; height: 70px;">
                 </a>
                 <p class="mb-0">Lineamientos Generales CIEyPCI</p>
             </div>
 
             <div>
-                <img src="/sistemas/images/comite/prin_inst.jpg">
+                <img src="../images/comite/prin_inst.jpg">
             </div>
 
             <?php
@@ -46,30 +46,30 @@ include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/header.php';
             for ($i = 0; $i < 6; $i++) {
                 if ($i < 3) {
                     echo '<div class="d-flex align-items-center my-3">
-            <img src="/sistemas/images/comite/' . $img[0] . '"alt="Ícono de Código" class="me-3" style="width: 70px; height: 70px;">
-            <a href="/sistemas/images/comite/' . $ligas[$i] . '">';
+            <img src="../images/comite/' . $img[0] . '"alt="Ícono de Código" class="me-3" style="width: 70px; height: 70px;">
+            <a href="../images/comite/' . $ligas[$i] . '">';
                 } else {
                     echo '<div class="d-flex align-items-center my-3">
-            <img src="/sistemas/images/comite/' . $img[1] . '"alt="Ícono de Código" class="me-3" style="width: 70px; height: 70px;">
-            <a href="/sistemas/images/comite/' . $ligas[$i] . '">';
+            <img src="../images/comite/' . $img[1] . '"alt="Ícono de Código" class="me-3" style="width: 70px; height: 70px;">
+            <a href="../images/comite/' . $ligas[$i] . '">';
                 }
                 echo 'Para descargar da click aquí
                 </a>
                 </div>';
                 if ($i == 2) {
                     echo '<div>
-                <img src="/sistemas/images/comite/COM_REGLAS.jpg">
+                <img src="../images/comite/COM_REGLAS.jpg">
                 </div>';
                 }
             }
             ?>
             <div>
-                <img src="/sistemas/images/comite/COM_VAL.jpg">
+                <img src="../images/comite/COM_VAL.jpg">
             </div>
 
             <div class="text-center">
-                <a href="/sistemas/images/documentos/codigo_conducta23.pdf" target="_blank">
-                    <img src="/sistemas/images/display/info_cetica.jpg" class="img-full-width">
+                <a href="../images/documentos/codigo_conducta23.pdf" target="_blank">
+                    <img src="../images/display/info_cetica.jpg" class="img-full-width">
                 </a>
             </div>
             <?php
@@ -77,11 +77,11 @@ include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/header.php';
             for ($i = 0; $i < 5; $i++) {
                 if ($i % 2 == 1) {
                     echo '<div>
-        <img src="/sistemas/images/comite/' . $img[$i] . '">
+        <img src="../images/comite/' . $img[$i] . '">
         </div>';
                 } else {
                     echo '<div>
-        <img src="/sistemas/images/comite/' . $img[$i] . '" style="width: 70px; height: 70px;">
+        <img src="../images/comite/' . $img[$i] . '" style="width: 70px; height: 70px;">
         </div>';
                 }
             } ?>
@@ -89,5 +89,5 @@ include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/header.php';
     </article>
 </body>
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/sistemas/views/footer.php';
+include 'footer.php';
 ?>
