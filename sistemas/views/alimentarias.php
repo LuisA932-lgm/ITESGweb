66eb161ef008875fef5608aced13daf3a80596e9
ferch0xX <ferchoperezri2@gmail.com>
2024-11-21 08:48:03 -0600

    Add files via upload

---
+++ sistemas/views/alimentarias.php
@@ -0,0 +1,185 @@
+<?php
+include ("header.php");
+?>
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <title>Ingeniería en Industrias Alimentarias</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style>
+        .article-body h3 {
+            color: #039;
+            font-family: 'Lato', sans-serif;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .article-body h4 {
+            color: #b5812d;
+            font-family: 'Lato', sans-serif;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .iconos {
+            display: flex;       
+            justify-content: flex-end; 
+            list-style: none;   
+            padding-top: 30px;   
+        }
+        .article-body p {
+            color: #333;
+            font-family: 'Lato', sans-serif;
+            font-size: 24px;
+            font-weight: normal;
+        }
+        .article-body li{
+            color: #333;
+            font-family: 'Lato', sans-serif;
+            font-size: 24px;
+        }
+    </style>
+</head>
+
+<body class="green align-ltr page-id-476 com-content-article chrome breakpoint-1200">
+    <div class="container">
+
+        <!--Start Main Body-->
+        <section id="main" class="row" style="padding: 0px">
+            <section id="mainbody" role="main" class="grid12 offset0 clearfix" style="min-height: 1915px; padding: 0px;">
+                <section id="component" role="article" class="clearfix" style="padding: 0px;">
+                    <div class="block">
+                        <article class="article fulltext ">
+                            <section class="actions" style="padding: 0px;">
+                            <ul class="iconos">
+                                    <li class="print-icon" style="margin-left: 15px;">
+                                        <a href="formulario-imprimir.html" title="Imprimir">
+                                            <i class="fas fa-print" style="color: #24398E;"></i> 
+                                        
+                                    </li>
+                                    <li class="email-icon" style="margin-left: 15px;">
+                                        <a href="formulario-correo.html" title="Enviar correo">
+                                            <i class="fas fa-envelope" style="color: #24398E"></i> 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </section>
+                            <section class="article-body" style="padding: 0px; width: 1400px; height: 3800;">
+                                <p align="center">
+                                    <img src="../images/headers/h_industrias.png" style="width: 1050px; height: 143; margin-right: 250px;">
+                                </p>
+                                <table width="85%" align="center">
+                                    <tbody>
+                                        <tr>
+                                            <td style="padding-right:30px" colspan="2">
+                                                <h3>OBJETIVO</h3>
+                                                <p>Formar profesionistas con sólidos conocimientos científicos y tecnológicos capaces de desarrollar, mejorar y producir alimentos inocuos e innovadores; bajo un esquema ético y sustentable acorde a las necesidades de la sociedad en el ámbito regional, nacional e internacional.
+                                                </p>
+                                                <h3>PERFIL DE INGRESO </h3>
+                                                <p>Intereses:</p>
+                                                <li>Interés por el estudio de la química.</li>
+                                                <li>En la transformación de alimentos.</li>
+                                                <li>Gusto por la investigación.</li>
+                                                <p></p>
+                                                <p>Conocimientos:</p>
+                                                <li> Biología, Física, Química, Álgebra, Trigonometría.</li>
+                                                <p></p>
+                                                <p>Habilidades: </p>                                                
+                                                    <li>Pensamiento crítico y creatividad.</li>
+                                                    <li>Capacidad de observación, análisis y síntesis.</li>
+                                                    <li>Capacidad para trabajar en equipo.</li>
+                                                    <li>Capacidad de emprendimiento.</li>
+                                                    <li>Habilidades de comunicación oral y escrita.</li>      
+                                                <p></p>                                          
+                                                <h3>PERFIL DE EGRESO</h3>
+                                                <p>El egresado de la carrera de Ingeniería en Industrias Alimentarias será capaz de diseñar, seleccionar, estandarizar y controlar la cadena de producción de productos alimentarios desde una perspectiva sustentable.</p>                                              
+                                                <h3>CAMPO LABORAL</h3>
+                                                <p><li>Empresas de procesamiento de bebidas y productos alimentarios.</li>
+                                                    <li>Consultoría de Ingeniería y calidad alimentaria.</li>
+                                                    <li>Sector público, organizaciones relacionadas con la producción, conservación, calidad y distribución de alimentos.</li>
+                                                    <li>Organismos certificadores y Centros de investigación.</li>
+                                                    <li>Sector educativo.</li>
+                                                    <li>Emprendiento de proyectos productivos.</li>
+                                                <p></p>  
+                                                <h3>MISIÓN</h3>
+                                                <p>Formar profesionales en ingeniería en industrias alimentarias capaces de formular y desarrollar proyectos industriales, de investigación, tecnológicos, innovación y emprendimiento que propongan acciones para satisfacer las necesidades alimentarias de la sociedad, fomentando el trabajo multidisciplinario con fundamento en la calidad, seguridad y sustentabilidad, a través de un modelo educativo humanístico basado en competencias.</p>
+                                                <h3>VISIÓN</h3>
+                                                <p>Al 2025 el programa de Ingeniería en Industrias Alimentarias del ITESG está acreditado y es reconocido como formador de profesionales líderes de la ingeniería capaces de ejercer su profesión de manera íntegra. Se vincula con organizaciones públicas, privadas y educativas de la región para la colaboración en el desarrollo de proyectos de mejora e innovación del sector Alimentario.</p>
+                                                <h3>VALORES</h3>
+                                                <p></p>
+                                                <li>
+                                                    <b>Disciplina</b>
+                                                </li>
+                                                <p></p>
+                                                <li><b>Eficiencia</b>
+                                                </li>
+                                                <p></p>
+                                                <li><b>Profesionalismo</b>
+                                                </li>
+                                                <p></p>
+                                                <li><b>Innovación</b>
+                                                </li>
+                                                <p></p>
+                                                <h3>PLAN DE ESTUDIOS</h3>
+                                                <p>El plan de estudios tiene una duración de 9 semestres con un total de 260 créditos.
+                                                Durante la trayectoria académica se cursan asignaturas de ciencias básicas, ciencias de la ingeniería, Ingeniería aplicada y cursos complementarios acordes al perfil de egreso del estudiante.</p>
+                                            </td>
+                                            <td valign="top" width="290px" rowspan="3">
+                                                <p><img src="../images/articulos/alimentarias_v.jpg"></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="400px" style="padding:0 20px" align="center">
+                                                <h4>Retícula IIAL 2010-219</h4>
+                                                <div class="alert alert-success">
+                                                    <a href="/images/documentos/ret_igem2009.pdf" target="_blank">
+                                                        <img src="../images/desc_pdf.png" height="60px" width="50px">
+                                                        <p style="color: darkblue;"> Descarga la retícula aquí</p>  
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td width="400px" style="padding:0 20px" align="center">
+                                                <h4>Flyer IIAL</h4>
+                                                <div class="alert alert-success">
+                                                    <a href="https://drive.google.com/file/d/14wka8y3xccQmQmtlvK6Em91dsdVDtah4/view?usp=sharing" target="_blank">
+                                                        <img src="../images/desc_pdf.png" height="60px" width="50px">
+                                                        <p style="color: darkblue;"> Descarga el Flyer aquí</p>  
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 20px" align="center">&nbsp;</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </section>
+                        </article>
+                    </div>
+                </section>
+            </section>
+        </section>
+        <!--End Main Body-->
+
+        <!--Start Main-Bottom Modules-->
+        <section id="main-bottom" class="row" style="padding: 20px;">
+            <div class="grid12 column first last ex-odd mainbottom-1">
+                <div class="block module mod-108 no-title clearfix" style="min-height: 138px;">
+                    <div class="content">
+                        <div class="custom">
+                            <p align="center"><img src="../images/logos/todos.jpg"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End Main Bottom Modules--> 
+    </div>
+</body
+
+<?php
+    include ("footer.php");
+?>
+
+</html>
---
+++ sistemas/views/sistemas.php
@@ -0,0 +1,193 @@
+<?php
+include ("header.php");
+?>
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <title>Ingeniería en Sistemas Computacionales</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style>
+        .article-body h3 {
+            color: #039;
+            font-family: 'Lato', sans-serif;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .article-body h4 {
+            color: #b5812d;
+            font-family: 'Lato', sans-serif;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .iconos {
+            display: flex;       
+            justify-content: flex-end; 
+            list-style: none;   
+            padding-top: 30px;   
+        }
+        .article-body p {
+            color: #333;
+            font-family: 'Lato', sans-serif;
+            font-size: 24px;
+            font-weight: normal;
+        }
+        .article-body li{
+            color: #333;
+            font-family: 'Lato', sans-serif;
+            font-size: 24px;
+        }
+    </style>
+</head>
+
+<body class="green align-ltr page-id-476 com-content-article chrome breakpoint-1200">
+    <div class="container">
+
+        <!--Start Main Body-->
+        <section id="main" class="row" style="padding: 0px">
+            <section id="mainbody" role="main" class="grid12 offset0 clearfix" style="min-height: 1915px; padding: 0px;">
+                <section id="component" role="article" class="clearfix" style="padding: 0px;">
+                    <div class="block">
+                        <article class="article fulltext ">
+                            <section class="actions" style="padding: 0px;">
+                            <ul class="iconos">
+                                    <li class="print-icon" style="margin-left: 15px;">
+                                        <a href="formulario-imprimir.html" title="Imprimir">
+                                            <i class="fas fa-print" style="color: #24398E;"></i> 
+                                        
+                                    </li>
+                                    <li class="email-icon" style="margin-left: 15px;">
+                                        <a href="formulario-correo.html" title="Enviar correo">
+                                            <i class="fas fa-envelope" style="color: #24398E"></i> 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </section>
+                            <section class="article-body" style="padding: 0px; width: 1400px; height: 3800;">
+                                <p align="center">
+                                    <img src="../images/headers/h_sistemas.png" style="width: 1050px; height: 143; margin-right: 250px;">
+                                </p>
+                                <table width="85%" align="center">
+                                    <tbody>
+                                        <tr>
+                                            <td style="padding-right:30px" colspan="2">
+                                                <h3>OBJETIVO</h3>
+                                                <p>Formar profesionistas líderes con visión estratégica y amplio sentido ético; capaz de diseñar, desarrollar, implementar y administrar tecnología computacional para aportar soluciones innovadoras en beneficio de la sociedad; en un contexto global, multidisciplinario y sostenible.
+                                                </p>
+                                                <h3>PERFIL DE INGRESO </h3>
+                                                <p>El aspirante debe reunir los conocimientos, habilidades y actitudes necesarias para cursar una ingeniería en el nivel superior, haber concluido los estudios de bachillerato en el nivel medio superior para responder adecuadamente a las exigencias de los planes de estudio que ofrece el ITESG.</p>
+                                                <p>Conocimientos básicos:</p>                                                
+                                                    <li>Ciencias básicas: Matemáticas, Física y Química</li>
+                                                    <li>Computación básica</li>
+                                                <p></p>
+                                                <p>Habilidades: </p>                                                
+                                                    <li>Capacidad para razonar matemáticamente.</li>
+                                                    <li>Creatividad y capacidad de emprender.</li>
+                                                    <li>Aplicar el ingenio en la resolución de problemas.</li>
+                                                    <li>Capacidad de análisis y síntesis</li>
+                                                    <li>Tecnológicas básicas</li>      
+                                                <p></p>                                          
+                                                <h3>PERFIL DE EGRESO</h3>                                               
+                                                <ol>
+                                                    <li>Implementa aplicaciones computacionales para solucionar problemas de diversos contextos, integrando diferentes tecnologías, plataformas o dispositivos.</li>
+                                                    <p></p>
+                                                    <li>Diseña, desarrolla y aplica modelos computacionales para solucionar problemas, mediante la selección y uso de herramientas matemáticas.</li>
+                                                    <p></p>
+                                                    <li>Diseña e implementa interfaces para la automatización de sistemas de hardware y desarrollo del software asociado.</li>
+                                                    <p></p>
+                                                    <li>Coordina y participa en equipos multidisciplinarios para la aplicación de soluciones innovadoras en diferentes contextos.</li>
+                                                    <p></p>
+                                                    <li>Diseña, implementa y administra bases de datos optimizando los recursos disponibles, conforme a las normas vigentes de manejo y seguridad de la información.</li>
+                                                    <p></p>
+                                                    <li>Desarrolla y administra software para apoyar la productividad y competitividad de las organizaciones cumpliendo con estándares de calidad.</li>
+                                                    <p></p>
+                                                    <li>Evalúa tecnologías de hardware para soportar aplicaciones de manera efectiva.</li>
+                                                    <p></p>
+                                                    <li>Detecta áreas de oportunidad empleando una visión empresarial para crear proyectos aplicando las Tecnologías de la Información y Comunicación.</li>
+                                                    <p></p>
+                                                    <li>Diseña, configura y administra redes de computadoras para crear soluciones de conectividad en la organización, aplicando las normas y estándares vigentes.</li>
+                                                </ol>
+                                                <h3>CAMPO LABORAL</h3>
+                                                <p><li>Industria 4.0 instalada en la región.</li>
+                                                    <li>Sectores especializados en tecnologías de Información y Telecomunicaciones</li>
+                                                    <li>En cualquier sector y sea industrial, gubernamental o de servicios</li>
+                                                <p></p>  
+                                                <h3>MISIÓN</h3>
+                                                <p>La carrera de Ingeniería en Sistemas Computacionales del ITESG forma ingenieros con un enfoque integral mediante un modelo basado en competencias fomentando el desarrollo tecnológico, investigación y proyectos multidisciplinarios en el área de Tecnologías de Información y comunicación, que contribuyen al impulso social, económico y sustentable del entorno.</p>
+                                                <h3>VISIÓN</h3>
+                                                <p>La carrera de Ingeniería en Sistemas Computacionales es un programa académico reconocido por su calidad en la formación de profesionales que aplican herramientas y técnicas esenciales en el área de Tecnologías de Información y Comunicaciones en su ámbito laboral, atendiendo la demanda del sector productivo y social a nivel regional, nacional e internacional.</p>
+                                                <h3>VALORES</h3>
+                                                <p></p>
+                                                <p>La comunidad tecnológica de la Ingeniería en Sistemas Computacionales, nos comprometemos a cumplir con los siguientes valores, que deben regir siempre nuestra conducta:</p>
+                                                <li>
+                                                    <b>Responsabilidad: </b>Cumplir con las funciones y tareas, individual y grupalmente, con un sentido de compromiso y un enfoque centrado en la calidad.
+                                                </li>
+                                                <p></p>
+                                                <li><b>Veracidad: </b>Actuar de conformidad con la verdad, con honestidad y transparencia en la ejecución de nuestros trabajos.
+                                                </li>
+                                                <p></p>
+                                                <li><b>Profesionalismo: </b>Ser ético, respetuoso, competente y mantenerse actualizado constantemente. </li>
+                                                <p></p>
+                                                <h3>PLAN DE ESTUDIOS</h3>
+                                                <p>El Plan de Estudios vigente en la carrera de Ingeniería en Sistemas Computacionales tiene la clave ISIC-2010-224, ha estado vigente desde agosto de 2010, acorde al oficio No. 513.1/1637/10, emitido por la dirección general de educación tecnológica, aplica para todos los estudiantes inscritos corresponde a 260 créditos que se componen de: 210 (estructura genérica 49 materias), 25 (módulo de especialidad 5 materias), 10 (programa de residencias), 10 (servicio social), 5 (actividades complementarias).</p>
+                                            </td>
+                                            <td valign="top" width="290px" rowspan="3">
+                                                <p><img src="../images/articulos/sistemas_v.jpg"></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="400px" style="padding:0 20px" align="center">
+                                                <h4>Retícula ISIC 2010-224</h4>
+                                                <div class="alert alert-success">
+                                                    <a href="/images/documentos/ret_igem2009.pdf" target="_blank">
+                                                        <img src="../images/desc_pdf.png" height="60px" width="50px">
+                                                        <p style="color: darkblue;"> Descarga la retícula aquí</p>  
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td width="400px" style="padding:0 20px" align="center">
+                                                <h4>Flyer ISIC</h4>
+                                                <div class="alert alert-success">
+                                                    <a href="https://drive.google.com/file/d/14wka8y3xccQmQmtlvK6Em91dsdVDtah4/view?usp=sharing" target="_blank">
+                                                        <img src="../images/desc_pdf.png" height="60px" width="50px">
+                                                        <p style="color: darkblue;"> Descarga el Flyer aquí</p>  
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 20px" align="center">&nbsp;</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </section>
+                        </article>
+                    </div>
+                </section>
+            </section>
+        </section>
+        <!--End Main Body-->
+
+        <!--Start Main-Bottom Modules-->
+        <section id="main-bottom" class="row" style="padding: 20px;">
+            <div class="grid12 column first last ex-odd mainbottom-1">
+                <div class="block module mod-108 no-title clearfix" style="min-height: 138px;">
+                    <div class="content">
+                        <div class="custom">
+                            <p align="center"><img src="../images/logos/todos.jpg"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End Main Bottom Modules--> 
+    </div>
+</body
+
+<?php
+    include ("footer.php");
+?>
+
+</html>
