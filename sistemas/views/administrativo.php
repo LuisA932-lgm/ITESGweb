a75b424f9d2f65a1ee46dde3fb63373e533955ba
Shiza <cesarajc2002@gmail.com>
2024-11-22 08:07:52 -0600

    Update administrativo.php

--- sistemas/views/administrativo.php
+++ sistemas/views/administrativo.php
@@ -12,7 +12,7 @@ include 'header.php';
 
 <body>
     <article class="article fulltext ">
-        <section class="article-body">
+        <section class="article-body1">
             <div class="text-center">
                 <img src="../images/headers/h_directorio.jpg">
             </div>
