38b1fe85639c7a0e076d103ade922529ce150542
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-22 12:13:52 -0600

    d1

--- sistemas/Informacion_Financiera/2023.php
+++ sistemas/Informacion_Financiera/2023.php
@@ -1,27 +1,354 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ejercicios Fiscales 2023</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios Fiscales 2023</title>
+    <link rel="stylesheet" href="../sistemas/css/gobmx.css">
+    <link rel="stylesheet" href="../sistemas/css/style.css">
+    <link rel="stylesheet" href="../sistemas/css/responsive.css">
+    <link rel="stylesheet" href="../sistemas/css/theme-vendors.min.css">
   </head>
   <body>
   <style>
-    .sub{
+    .sub {
         color: rgb(181, 129, 45);
         font-size: 24px;
+        text-align: left;
         font-weight: bold;
-        margin-bottom: 1px;
-        margin-left: 10px;
+        margin-bottom: 20px;
+        margin-left: 15px;
     }
-    .h3{
-        font-size: 20px;
-        margin-left: 10px;
+
+    .sub2 {
+        color: rgb(181, 129, 45);
+        font-size: 24px;
+        text-align: left;
+        font-weight: bold;
+        margin-bottom: 20px;
+        margin-left: 15px;
+    }
+
+    .sub3 {
+        color: rgb(181, 129, 45);
+        font-size: 24px;
+        text-align: left;
+        font-weight: bold;
+        margin-bottom: 20px;
+        margin-left: 15px;
+    }
+
+    .lin {
+        font-size: 18px;
+        margin-top: -25px;
+        margin-left: 15px;
+        font-weight: bold;
+    }
+
+    .Trans {
+        color: black;
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: -20px;
+        font-weight: bold;
+    }
+
+    .ing {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: -20px;
+        font-weight: bold;
+    }
+
+    .Egre {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: -20px;
+        font-weight: bold;
+    }
+
+    .PR {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .ID {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IC {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IP {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IPro {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IDB {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .EJ {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IDF {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .IDFI {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .MCG {
+        font-size: 18px;
+        margin-left: 15px;
+        margin-top: 10px;
+        font-weight: bold;
+    }
+
+    .FE {
+        font-size: 16px;
+        color: black;
+        margin-left: 15px;
+        margin-top: 20px;
+    }
+
+    .RI {
+        font-size: 17px;
+        color: black;
+        margin-left: 15px;
+        margin-top: 20px;
+    }
+
+    .LICF {
+        margin-left: 100px;
+        color: black;
+
+    }
+
+    .CPC {
+        margin-left: 430px;
+        color: black;
+        margin-top: -90px;
+    }
+
+    .CPCL {
+        margin-left: 800px;
+        color: black;
+        margin-top: -90px;
+    }
+
+    .INGE {
+        margin-left: 100px;
+        color: black;
+        margin-top: 10px;
+
+    }
+
+    .Enlaces {
+        font-size: 17px;
+        color: black;
+        margin-left: 15px;
+    }
+
+    .consejo {
+        margin-left: 15px;
+    }
+
+    .Armo {
+        margin-left: 15px;
+    }
+
+
+    #t1 {
+        width: 800px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 20px;
+        margin-left: 300px;
+    }
+
+    .table2 {
+        width: 800px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 20px;
+        margin-left: 300px;
+    }
+
+    .table3 {
+        width: 800px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table4 {
+        width: 800px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table5 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table6 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table7 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table8 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table9 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table10 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table11 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table12 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+    }
+
+    .table13 {
+        width: 1000px;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin-top: 13px;
+        margin-left: 300px;
+
     }
 </style>
 <body>
-    <h2 class="sub">Información Anual 2023</h2>
-    <a href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank"><h3 class="h3" style="color:  #095ba3 ">Liga de Cuenta Pública 2023 (SFIyA)</h3></a>
-    <h3 class= "h3">Transparencia y Difusión de la Información Financiera LGCG</h3>
+<h2 class="sub">Información Anual</h2>
+    <a href="https://portaldgi.guanajuato.gob.mx/CuentaPublica/public/main" target="_blank">
+
+        <h3 class="lin" style="color:  #095ba3 ">Liga de Cuenta Pública 2023 (SFIyA)</h3>
+    </a>
+
+    <h5 class="Trans">Transparencia y Difusión de la Información Financiera LGCG</h5>
+
+    <h3 class="ing" style="color: #FB008A">Ingreso</h3>
+
+    <table width="70%" align="center" class="table1 " id="t1">
+        <thead style="background-color: #0073D1; color: #FFFFFF">
+            <tr>
+                <th>Documento</th>
+                <th>Descarga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%; color:black">Iniciativa de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/Iniciativa_de_Ingresos_2024.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%; color:black">Información Adicional a la Iniciativa de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/1.INF.%20ADICIONAL%20INIC.%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="https://www.itesg.edu.mx/images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%; color:black">Dictámenes Acuerdo y Actas </td>
+                <td style="width: 20%;">
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/Dictamenes_Acuerdo_y_Actas_ingreso_2024.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 80%; color:black">Ley de Ingresos </td>
+                <td style="width: 20%;">
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/Ley_de_Ingresos_2024.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                </td>
+            </tr>
+            <tr style="background-color: #f8f8f9;">
+                <td style="width: 80%; color:black">Calendario de Ingresos</td>
+                <td style="width: 20%;">
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="https://www.itesg.edu.mx/LGCGyLDF/EJ2024/2.CALENDARIO%20INGRESOS%20ITSG%202024.xlsx" target="_blank"> <img src="https://www.itesg.edu.mx/images/excel_1.jpg" title="Archivo Excel"></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
 </body>
 </html>
