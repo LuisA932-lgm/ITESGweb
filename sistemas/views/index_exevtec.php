6c7efcbf9f8dce52de651337a9399a33d72a02a5
Luis01-AE <157388343+LuisA932-lgm@users.noreply.github.com>
2024-11-25 14:54:25 -0600

    Update index_exevtec.php

--- sistemas/views/index_exevtec.php
+++ sistemas/views/index_exevtec.php
@@ -11,17 +11,17 @@ include ("header.php");
     <link rel="stylesheet" href="../css/index_exevtec.css">
 </head>
 <body>
-   <img class="imagen_1" src="../images/convocatorias/ADM_EJ_2025.jpg" > 
+   <img class="imagen_1" src="../images/convocatoria/ADM_EJ_2025.jpg" > 
     <br>
     <br>
     <h5>Convocatoria Examen 6 de Diciembre</h5>
     <div class="alert alert-info">
-        <a href="../images/convocatorias/ADM_EJ_2025.pdf" target="_blank"> <center>
+        <a href="../images/convocatoria/ADM_EJ_2025.pdf" target="_blank"> <center>
         <img src="../images/iconos/desc_pdf.png" width="52px" height="50px" >
         <u class="uconv"><p class="pconv">Descargar convocatoria</p></center></a></style>
     </div>
     <p align="center">
-        <img src="../images/convocatorias/todos.jpg">
+        <img src="../images/convocatoria/todos.jpg">
     </p>
  
 </body>
