67a47c8794ec6a506422b9bab67f9a403f6ddc87
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-11-25 12:20:22 -0600

    D1

--- sistemas/views/Informacion_Financiera/2023.php
+++ sistemas/views/Informacion_Financiera/2023.php
@@ -682,7 +682,7 @@
         </tbody>
     </table>
     <h3 class="IP" id="InfP" style="color: #FB008A">Información Presupuestaria</h3>
-    <table align="center" width="90%" class="table5">
+    <table align="center" width="90%" class="table5" id="t5">
                     <thead style="background-color: #0073D1; color: #FFFFFF"><tr><th>Documento</th><th>1er. Trim.</th><th>2do. Trim.</th><th>3er. Trim.</th><th>4to. Trim.</th></tr></thead>
                     <tbody>
                     <tr>
