53cbde2bdbc9a5d2067025cd8958bec389e07848
julioo78 <157434139+julioo78@users.noreply.github.com>
2024-12-01 17:40:21 -0600

    q2

--- sistemas/views/LGCG_y_LDF.php
+++ sistemas/views/LGCG_y_LDF.php
@@ -261,7 +261,7 @@ function loadContent(year) {
             }
         },
         error: function() {
-            $('#contentDiv').html('<img src="../images/1.png" style="display:block; margin: 0 auto;"/> <p> Página en mantenimiento  </p>');
+            $('#contentDiv').html('<img src="../images/1.png" style="display:block; margin: 0 auto;"/> <p> Página en mantenimiento. </p>');
         }
     });
 }
