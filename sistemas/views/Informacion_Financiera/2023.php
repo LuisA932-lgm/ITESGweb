324283cce7339620ef2dae7a0d1788255147cd20
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-12-01 14:20:41 -0600

    D1

--- sistemas/views/Informacion_Financiera/2023.php
+++ sistemas/views/Informacion_Financiera/2023.php
@@ -350,8 +350,8 @@
             <tr style="background-color: #f8f8f9;">
                 <td style="width: 80%; color:black">Calendario de Ingresos</td>
                 <td style="width: 20%;">
-                    <a href="http://localhost/ITESGweb/sistemas/views/Informacion_Financiera/LGCGyLDF/EJ2023/CI_ITSG_2023.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
-                    <a href="http://localhost/ITESGweb/sistemas/views/Informacion_Financiera/LGCGyLDF/EJ2023/CI_ITSG_2023.xlsx" target="_blank"> <img src="https://www.itesg.edu.mx/images/excel_1.jpg" title="Archivo Excel"></a>
+                    <a href="../views/Informacion_Financiera/LGCGyLDF/EJ2023/CI_ITSG_2023.pdf" target="_blank"> <img src="https://www.itesg.edu.mx/images/pdf_1.jpg" title="Archivo PDF"></a>
+                    <a href="../views/Informacion_Financiera/LGCGyLDF/EJ2023/CI_ITSG_2023.xlsx" target="_blank"> <img src="https://www.itesg.edu.mx/images/excel_1.jpg" title="Archivo Excel"></a>
                 </td>
             </tr>
         </tbody>
