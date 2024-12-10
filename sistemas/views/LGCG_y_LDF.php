00e8cff63a3ad675324a33c2faef667c1de1540c
MARIO <JM.SANCHEZ.TORRES@UGTO.MX>
2024-12-10 00:39:06 -0600

    D1

--- sistemas/views/LGCG_y_LDF.php
+++ sistemas/views/LGCG_y_LDF.php
@@ -232,13 +232,13 @@ fieldset[disabled] .btn-primary:hover {
           <div class="btn-group btn-group-lg" role="group">
    
       
-      <button type="button" class=" btn-year.selected btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2018')">2018</button>
+        <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2018')">2018</button>
         <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2019')">2019</button>
         <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2020')">2020</button>
         <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2021')">2021</button>
         <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2022')">2022</button>
         <button type="button" class="btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2023')">2023</button>
-        <button type="button" class="btn-year.selected btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2024')">2024</button>
+        <button type="button" class="btn-year selected btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2024')">2024</button>
   </center>
   <div id="contentDiv">
     
@@ -252,8 +252,13 @@ $(document).ready(function() {
 });
 
 function loadContent(year) {
+    // Cambiar la clase 'selected' al botón correspondiente
+    $('.btn-year').removeClass('selected');
+    $(`button[onclick="loadContent('${year}')"]`).addClass('selected');
+
+    // Realizar la llamada AJAX
     $.ajax({
-        url: `http://localhost/ITESGweb/sistemas/views/Informacion_Financiera/${year}.php`, 
+        url: `http://localhost/ITESGweb/sistemas/views/Informacion_Financiera/${year}.php`,
         type: "GET",
         success: function(response) {
             if (response.trim() === "") {
@@ -267,6 +272,7 @@ function loadContent(year) {
         }
     });
 }
+
 </script>
 
 <?php
