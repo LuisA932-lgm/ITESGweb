a5e1e219f5307b996f436f0b9af6ad1d869cab53
“JMario-S” <jm.sanchez.torres@ugto.mx>
2024-12-09 20:59:31 -0600

    D1

--- sistemas/views/LGCG_y_LDF.php
+++ sistemas/views/LGCG_y_LDF.php
@@ -234,6 +234,8 @@ fieldset[disabled] .btn-primary:hover {
 <button type="button" class="btn-year.selected btn btn-default" style="color: #2EC3F8; font-size: 22px" onclick="loadContent('2024')">2024</button>
 </center>
 <div id="contentDiv">
+console.log("URL solicitada: http://localhost/ITESGWEB/sistemas/views/Informacion_Financiera/" + year + ".php");
+
 </div>
 </body>
 </html>
