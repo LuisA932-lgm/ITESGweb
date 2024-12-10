d8feed5fde6dc9784a235f974b8bc2743e1a914b
“JMario-S” <jm.sanchez.torres@ugto.mx>
2024-12-09 20:32:23 -0600

    D1

--- sistemas/views/LGCG_y_LDF.php
+++ sistemas/views/LGCG_y_LDF.php
@@ -1,6 +1,15 @@
 <?php include ("header.php");
 
-?>< !DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Ejercicios Fiscales 2021</title></head><body><style>.Ti {
+?>
+< !DOCTYPE html>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios Fiscales</title>
+    </head>
+    <body>
+        <style>
+        .Ti {
     color: rgb(181, 129, 45);
     font-family: Lato, sans-serif;
     font-size: 21px;
