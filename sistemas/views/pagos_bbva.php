9bda2f71b7c1125ddd2a15bd986efdb6c33ba71a
ferch0xX <ferchoperezri2@gmail.com>
2024-12-10 11:04:44 -0600

    Add files via upload

---
+++ sistemas/views/pagos_bbva.php
@@ -0,0 +1,40 @@
+<?php
+include ("header.php");
+?>
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <title>Pagos BBVA</title>
+</head>
+
+<body class="green align-ltr page-id-476 com-content-article chrome breakpoint-1200">
+    <div class="container">
+        <!--Start Main Body-->
+        <section id="main" class="row" style="margin: 0; padding: 0;">
+            <section id="mainbody" role="main" class="grid12 offset0 clearfix" style="min-height: 100vh; padding: 0;">
+                <section id="component" role="article" class="clearfix" style="padding: 0;">
+                    <div class="block">
+                        <article class="article fulltext">
+                            <section class="article-body" style="padding: 0; max-width: 100%; background-color: white;">
+                                <div style="text-align: center; padding-top: 40px;">
+                                    <img src="../images/documentos/pagossbbva.jpg" 
+                                         style="max-width: 90%; height: auto; display: block; margin: 0 auto;">
+                                         <p align="center"><img src= "../images/logos/todos.jpg" ></p>
+                                </div> 
+                            </section>
+                        </article>
+                    </div>
+                </section>
+            </section>
+        </section>
+        <!--End Main Body-->
+    </div>
+</body>
+
+
+<?php
+    include ("footer.php");
+?>
+
+</html>
